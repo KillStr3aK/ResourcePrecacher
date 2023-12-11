d1d0a90a57037b17fc6b044102148523150dcf39
KillStr3aK <statser07@gmail.com>
2023-12-11 18:17:34 +0100

    removed double key

--- Services/PluginMigrations.cs
+++ Services/PluginMigrations.cs
@@ -6,7 +6,6 @@
         {
             this.AddMigration(1, 2, "You have to update the 'Windows' key inside 'CreatePrecacheContextSignature', and change the 'Version' to 2. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
             this.AddMigration(2, 3, "You have to add a 'Log' key from your configuration, and change the 'Version' to 3. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
-            this.AddMigration(1, 3, "Your config is very old. Grab the latest one from the plugin repository and dont forget to change the 'Version' to 3. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
         }
 
         public bool HasInstruction(int fromVersion, int toVersion)
