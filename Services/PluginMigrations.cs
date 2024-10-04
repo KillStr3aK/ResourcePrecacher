ba1b6109ce80e4eb9630a656493e14f8da7a6eed
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-10-04 23:07:03 +0200

    chore: added migration text

--- Services/PluginMigrations.cs
+++ Services/PluginMigrations.cs
@@ -8,6 +8,7 @@
             this.AddMigration(2, 3, "You have to add a 'Log' key from your configuration, and change the 'Version' to 3. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
             this.AddMigration(3, 4, "[WINDOWS ONLY] Updated signatures, redownload the latest version. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
             this.AddMigration(4, 5, "Changed functionality, remove signature related config values. (The latest example configuration is always available at: https://github.com/KillStr3aK/ResourcePrecacher/blob/master/public/addons/counterstrikesharp/configs/plugins/ResourcePrecacher/ResourcePrecacher.json)");
+            this.AddMigration(5, 6, "You have to change the config 'Version' from 5 to 6. From version 6, you can upload your workshop vpks inside the 'Assets' folder and everything is precached automatically. (You can still use the 'ResourcesList' option in the config, but dont use both for the same or you get duplicate warnings)");
         }
 
         public bool HasInstruction(int fromVersion, int toVersion)
